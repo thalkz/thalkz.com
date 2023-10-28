7a0ac8dba639adee5a369c66093b82e560e7a238
Roland Lamidieu <roland.lamidieu@gmail.com>
2023-10-28 17:41:35 +0300

    fix: Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 # This will instantly update the content on thalkz.com
 # without needing to make a deployment of the server
 
-scp -r ./pages/* ubuntu@thalkz.com:/home/ubuntu/thalkz.com/pages
-scp -r ./images/* ubuntu@thalkz.com:/home/ubuntu/thalkz.com/images
+scp -r ./pages/* ubuntu@thalkz.com:/home/ubuntu/thalkz.com/pages/
+scp -r ./images/* ubuntu@thalkz.com:/home/ubuntu/thalkz.com/images/
