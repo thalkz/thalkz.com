619108253bb702befb48f76b50fa9cc799cddf79
Roland Lamidieu <roland.lamidieu@gmail.com>
2025-05-11 11:42:38 +0200

    fix build

--- syntax_highlight.go
+++ syntax_highlight.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
-	"github.com/gomarkdown/markdown/ast"
+
 	"github.com/alecthomas/chroma"
 	"github.com/alecthomas/chroma/formatters/html"
 	"github.com/alecthomas/chroma/lexers"
 	"github.com/alecthomas/chroma/styles"
+	"github.com/gomarkdown/markdown/ast"
 )
 
 var (
